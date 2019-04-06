afefab0774cb717c58e8e10b851b8098f7cb8e3c
sirish-git <sireesh_nitw@yahoo.co.in>
2019-04-06 16:53:59 +0530

    adaptive algo updated to read inputs from file

--- algo_opt_size_adaptive_selection/src/algo_defines.h
+++ algo_opt_size_adaptive_selection/src/algo_defines.h
@@ -6,17 +6,21 @@
 #define TARGET_FILE_SIZE	81
 #define QP_START_VAL		0
 
+/* Algo Macros */
+#define LONG_MAX			1099511627776 /* 2^40 */
+
 /* Error status */
 #define	SUCCESS				0
 #define FAILURE				-1
 
-/* Algo Macros */
-#define LONG_MAX			1099511627776 /* 2^40 */
-
-
 /* Structure for buffers */
 struct _buf_info
 {
+	/* Input buffers */
+	int		*inp_size_per_qp;
+	float	*inp_val_per_qp;
+
+	/* Processing buffers */
 	float	*val_buf1;
 	float	*val_buf2;
 	int		*parent_id_buf;
@@ -28,9 +32,3 @@ struct _buf_info
 int create_bufs(struct _buf_info *buf_info);
 void de_init(struct _buf_info *buf_info);
 int find_opt_size_adaptive(struct _buf_info *buf_info);
-
-/* Input info */
-/* Files size for each QP (flattened buffer) */
-extern int inp_size_per_qp[NUM_INP_FILES * NUM_QP_VALUES];
-/* Error value for each QP (flattened buffer) */
-extern float inp_val_per_qp[NUM_INP_FILES * NUM_QP_VALUES];
--- algo_opt_size_adaptive_selection/src/main.cpp
+++ algo_opt_size_adaptive_selection/src/main.cpp
@@ -12,10 +12,76 @@ void print_input_config()
 	printf("\n");
 }
 
-int main()
+int read_input(struct _buf_info *buf_info, FILE *fp_inp_size, FILE *fp_inp_val)
 {
+	int idx;
+	/* Files size for each QP (flattened buffer) */
+	int		*inp_size_per_qp;
+	/* Error value for each QP (flattened buffer) */
+	float	*inp_val_per_qp;
+
+	/* Assign buffer pointers */
+	inp_size_per_qp = buf_info->inp_size_per_qp;
+	inp_val_per_qp = buf_info->inp_val_per_qp;
+
+	/* read size file */
+	idx = 0;
+	while (!feof(fp_inp_size))
+	{
+		fscanf(fp_inp_size, "%d, ", &inp_size_per_qp[idx]);
+		idx++;
+	}
+	if (idx != (NUM_INP_FILES * NUM_QP_VALUES))
+	{
+		printf(" Error: Input is not of expected size\n");
+		return FAILURE;
+	}
+
+	/* read size file */
+	idx = 0;
+	while (!feof(fp_inp_val))
+	{
+		fscanf(fp_inp_val, "%f, ", &inp_val_per_qp[idx]);
+		idx++;
+	}
+	if (idx != (NUM_INP_FILES * NUM_QP_VALUES))
+	{
+		printf(" Error: Input is not of expected size\n");
+		return FAILURE;
+	}
+
+	return SUCCESS;
+}
+
+int main(char arg_c, char **arg_v)
+{
+	FILE *fp_inp_size, *fp_inp_val;
 	struct _buf_info buf_info;
 	int status;
+
+	if (arg_c < 3)
+	{
+		printf(" Incorrect input arguments\n");
+		printf(" Usage of the solution\n");
+		printf("	<.exe> <input size file> <input val file>\n");
+		return -1;
+	}
+
+	/* Open input size file */
+	fp_inp_size = fopen(arg_v[1], "r");
+	if (fp_inp_size == NULL)
+	{
+		printf(" Error: Input file open error\n");
+		return -1;
+	}
+	/* Open input value file */
+	fp_inp_val = fopen(arg_v[2], "r");
+	if (fp_inp_val == NULL)
+	{
+		printf(" Error: Input file open error\n");
+		fclose(fp_inp_size);
+		return -1;
+	}
 
 	print_input_config();
 
@@ -27,8 +93,28 @@ int main()
 		goto END;
 	}
 
+	/* Read input files */
+	status = read_input(&buf_info, fp_inp_size, fp_inp_val);
+	if (status != SUCCESS)
+	{
+		printf(" !! Input file reading failed\n");
+		goto END;
+	}
+
+	/* Find optimal solution */
 	find_opt_size_adaptive(&buf_info);
 
 END:
 	de_init(&buf_info);
+
+	if (fp_inp_size != NULL)
+	{
+		fclose(fp_inp_size);
+	}
+	if (fp_inp_val != NULL)
+	{
+		fclose(fp_inp_val);
+	}
+
+	return 0;
 }
